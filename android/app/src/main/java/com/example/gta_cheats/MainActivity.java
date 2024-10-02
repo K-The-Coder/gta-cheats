384b484eb1b41e1f16f6678eedf72f5fa4922331
Keketso Mabule <kekemabule19990216@gmail.com>
2024-10-02 15:32:45 +0200

    initial commit: started home screen

---
+++ android/app/src/main/java/com/example/gta_cheats/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.gta_cheats;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args)
+{
+  runApp(const CheatsApp());
+}
+
+class CheatsApp extends StatelessWidget {
+  const CheatsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'GTA Cheats App',
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+
+    );
+  }
+}
